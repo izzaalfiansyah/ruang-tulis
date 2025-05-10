6031ad6cc18b993baa9d49fde7d23f1e8bfa16d7
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2025-05-10 17:44:22 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<stype lang="scss">
+@use "./app.scss";
+</stype>
+
+<template>
+  <div class="min-h-screen flex items-center justify-center">
+    <div class="text-5xl font-bold text-primary">Ruang Kita</div>
+  </div>
+</template>
